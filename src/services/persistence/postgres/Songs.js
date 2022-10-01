00746447192e4b9ae048b1ce34929213bd06a2c1
erikhant <erikd.h49@gmail.com>
2022-10-01 23:13:09 +0700

    update: song service

--- src/services/persistence/postgres/Songs.js
+++ src/services/persistence/postgres/Songs.js
@@ -1,75 +1,74 @@
 const { Pool } = require('pg')
 const { nanoid } = require('nanoid')
-const { InvariantError } = require('../../../../common/exceptions')
+const { mapDBToModel } = require('../../../common/utils')
+const { InvariantError, NotFoundError } = require('../../../common/exceptions')
 
 class Songs {
-  constructor (pool) {
-    this._tableName = 'songs'
-    this._pool = pool
+  #name = 'song'
+  constructor () {
+    this._pool = new Pool()
   }
 
   async add ({ title, year, genre, performer, duration = null, albumId }) {
     const id = nanoid(16)
 
     const query = {
-      text: `INSERT INTO ${this._tableName} VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id`,
-      values: [id, title, year, genre, performer, duration != null ? duration : 'NULL', albumId]
+      text: `INSERT INTO ${this.#name} VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id`,
+      values: [id, title, year, genre, performer, duration !== null ? duration : 'NULL', albumId]
     }
-
     const result = await this._pool.query(query)
 
     if (!result.rows[0].id) {
-      throw new InvariantError('Catatan gagal ditambahkan')
+      return new InvariantError('Failed to add new song')
     }
 
     return result.rows[0].id
   }
 
   async getAll () {
-    const result = await this._pool.query('SELECT * FROM notes');
-    return result.rows.map(mapDBToModel);
+    const result = await this._pool.query(`SELECT * FROM ${this.#name}`)
+    return result.rows.map(mapDBToModel)
   }
 
-  async getNoteById(id) {
+  async getById (id) {
     const query = {
-      text: 'SELECT * FROM notes WHERE id = $1',
-      values: [id],
-    };
-    const result = await this._pool.query(query);
+      text: `SELECT * FROM ${this.#name} WHERE id = $1`,
+      values: [id]
+    }
+    const result = await this._pool.query(query)
 
     if (!result.rows.length) {
-      throw new NotFoundError('Catatan tidak ditemukan');
+      throw new NotFoundError({ entityName: 'song', fieldName: 'id', request: id })
     }
 
-    return result.rows.map(mapDBToModel)[0];
+    return result.rows.map(mapDBToModel)[0]
   }
 
-  async editNoteById(id, { title, body, tags }) {
-    const updatedAt = new Date().toISOString();
+  async update (id, { title, year, genre, performer, duration = null, albumId }) {
     const query = {
-      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
-      values: [title, body, tags, updatedAt, id],
-    };
+      text: `UPDATE ${this.#name} SET title = $1, year = $2 genre = $3, performer = $4, duration = $5, albumId = $6 WHERE id = $7 RETURNING id`,
+      values: [title, year, genre, performer, duration !== null ? duration : 'NULL', albumId, id]
+    }
 
-    const result = await this._pool.query(query);
+    const result = await this._pool.query(query)
 
     if (!result.rows.length) {
-      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+      throw new NotFoundError({ entityName: 'song', fieldName: 'id', request: id })
     }
   }
 
-  async deleteNoteById(id) {
+  async delete (id) {
     const query = {
-      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
-      values: [id],
-    };
+      text: `DELETE FROM ${this.#name} WHERE id = $1 RETURNING id`,
+      values: [id]
+    }
 
-    const result = await this._pool.query(query);
+    const result = await this._pool.query(query)
 
     if (!result.rows.length) {
-      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+      throw new NotFoundError({ entityName: 'song', fieldName: 'id', request: id })
     }
   }
 }
 
-module.exports = { Songs }
+module.exports = Songs
